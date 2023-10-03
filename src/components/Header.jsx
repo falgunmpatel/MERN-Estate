f3f32672b7605406cd988a9487f97a673c5991c2
Falgun Patel <falgunmpatel9123@gmail.com>
2023-10-03 18:16:25 +0530

    Create Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="bg-stone-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-stone-500">Sahajanand</span>
+            <span className="text-stone-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-stone-100 rounded-lg p-3 flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-stone-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-stone-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-stone-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="sm:inline text-stone-700 hover:underline">
+              Sign in
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
