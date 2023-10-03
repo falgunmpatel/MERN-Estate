84fe8b7a47e189db45e7f0aebdcd31ac6cecccf2
Falgun Patel <falgunmpatel9123@gmail.com>
2023-10-03 20:17:51 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
+
+
 
 const port = 3000;
 app.listen(port, () => console.log(`Server is running at port ${port}!`))
