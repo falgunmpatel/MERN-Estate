b63e5d1a006fa936fa0c8c7f1c71208bb3c168ae
Falgun Patel <falgunmpatel9123@gmail.com>
2023-10-03 21:50:47 +0530

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -23,3 +23,13 @@ app.listen(port, () => console.log(`Server is running at port ${port}!`));
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
