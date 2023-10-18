19183b80e5d010c32091658e9cba3c81f4e6479c
Falgun Patel <falgunmpatel9123@gmail.com>
2023-10-18 11:55:54 +0530

    complete update user functionality

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -4,12 +4,11 @@ import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
-  if (!token) {
-    return next(errorHandler(401, "Unauthorized"));
-  }
-
+  if (!token) return next(errorHandler(401, "Unauthorized"));
+  console.log("reached line 8");
   jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
     if (error) return next(errorHandler(403, "Forbidden"));
+
     req.user = user;
     next();
   });
