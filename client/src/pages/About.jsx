1e606913c28b4cf711633a702d3e860879941e62
Falgun Patel <falgunmpatel9123@gmail.com>
2023-11-12 00:44:18 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return <div className="p-20 px-4 max-w-6xl mx-auto">
+    <h1 className="text-3xl font-bold mb-4 text-slate-800">About Sahajanand Estate</h1>
+    <p className="mb-4 text-slate-700 text-justify">
+      Sahajanand Estate is leading real estate company that specializes in helping people buy, sell and rent their properties. We are a team of young and dynamic professionals who are highly skilled and experienced in the field of real estate. We are committed to providing the best services to our clients and help them find their dream home.
+    </p>
+    <p className="mb-4 text-slate-700 text-justify">
+      Our mission is to help our clients achieve their real estate goals by providing expert advice and guidance throughout the entire process. We have a wide range of properties available for you to choose from. Whether you are looking for a house, apartment or villa, we have something for everyone.
+    </p>
+    <p className="mb-4 text-slate-700 text-justify">
+      Our team of agents has a wealth of experience and knowledge of real estate market trends. We will work with you every step of the way to ensure that your needs are met and that you are satisfied with our services. We beleive that buying or selling a property should be an enjoyable experience, not a stressful one.
+    </p>
+  </div>;
 }
